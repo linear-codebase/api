fe99237b6e4008aa5c063d20f4265bdbd31faadf
João Coimbra <me@joaocoimbra.dev>
2025-10-24 10:52:44 -0300

    feat(lib): add JWT utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+type SendAuthLinkOptions = {
+  email: string
+  redirectUrl: string
+}
+
+export async function sendAuthLink({
+  email,
+  redirectUrl,
+}: SendAuthLinkOptions) {
+  // Implement sendAuthLink logic here
+}
