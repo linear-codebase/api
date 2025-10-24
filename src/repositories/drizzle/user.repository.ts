d9dcdc3cdb9479133b2f8217013cea23aa9b1b2e
João Coimbra <me@joaocoimbra.dev>
2025-10-24 10:51:44 -0300

    feat(repository): update user repository implementation

--- src/repositories/drizzle/user.repository.ts
+++ src/repositories/drizzle/user.repository.ts
@@ -3,29 +3,33 @@ import { db } from "@/database/client"
 import { schema } from "@/database/schema"
 import type { User, UserInsert } from "@/database/schema/users"
 import type { ID } from "@/shared/types/utils"
-import type { IUserRepository } from "../type/user"
+import type { IUserRepository } from "../types/user"
 
 export class DrizzleUserRepository implements IUserRepository {
   async create(data: UserInsert): Promise<User> {
     const user = await db.insert(schema.users).values(data).returning()
     return user[0]
   }
+
   async findAll(): Promise<User[]> {
     const users = await db.query.users.findMany()
     return users
   }
+
   async findById(id: ID): Promise<User | null> {
     const user = await db.query.users.findFirst({
       where: eq(schema.users.id, id),
     })
     return user ?? null
   }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = await db.query.users.findFirst({
       where: eq(schema.users.email, email),
     })
     return user ?? null
   }
+
   async update(id: ID, data: Partial<UserInsert>): Promise<User> {
     const user = await db
       .update(schema.users)
@@ -34,6 +38,7 @@ export class DrizzleUserRepository implements IUserRepository {
       .returning()
     return user[0]
   }
+
   async delete(id: ID): Promise<void> {
     await db.delete(schema.users).where(eq(schema.users.id, id))
   }
